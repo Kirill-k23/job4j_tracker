89fce573209f66e1f35921587fc78f1806bc3ad7
Kiril-k23 <kirillkleyn@gmail.com>
2023-05-06 16:11:13 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,25 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Номер " + id);
+        System.out.println("Имя " + name);
+        System.out.println();
+    }
+
     public int getId() {
         return id;
     }
@@ -18,5 +37,14 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static void main(String[] args) {
+        Item reloadFirst = new Item();
+        reloadFirst.printInfo();
+        Item reloadSecond = new Item("Kirill", 25);
+        reloadSecond.printInfo();
+        Item reloadThird = new Item("Vitaliy");
+        reloadThird.printInfo();
     }
 }
