2dede0be91b6baa4de8e86c228b0b94aed41b6aa
Kiril-k23 <kirillkleyn@gmail.com>
2023-05-10 22:50:09 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -12,14 +14,35 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
+            if (items[index].getId() == id) {
+                return items[index];
             }
         }
-        return rsl;
+        return null;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int count = 0;
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                rsl[count] = items[index];
+                count++;
+            }
+        }
+        return Arrays.copyOf(rsl, count);
+    }
+
+    public Item[] findAll(Item item) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                result[count] = items[i];
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
     }
 }
