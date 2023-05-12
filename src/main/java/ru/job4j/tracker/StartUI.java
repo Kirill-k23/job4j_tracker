6aac6794eced0000a931c91aefb9ae125d7c6455
Kiril-k23 <kirillkleyn@gmail.com>
2023-05-12 22:47:59 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,8 +4,12 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("Kirill", 24);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(item.getCreated().format(formatter));
+        System.out.println("Item:" + " "
+                + item.getCreated().format(formatter) + " "
+        + item.getId() + " "
+        + item.getName()
+        );
     }
 }
