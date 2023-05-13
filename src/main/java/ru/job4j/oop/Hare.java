4ec08e57185cc4692f2c97bd37738478c5f4cc0a
Kiril-k23 <kirillkleyn@gmail.com>
2023-05-13 17:56:08 +0300

    12. Enum  [#504960]

--- src/main/java/ru/job4j/oop/Hare.java
+++ src/main/java/ru/job4j/oop/Hare.java
@@ -1,7 +1,8 @@
 package ru.job4j.oop;
 
 public class Hare {
-    public void tryEat(Ball ball) {
-        ball.tryRun(false);
+    public void tryEat(Ball a) {
+        a.tryRun(false);
     }
 }
+
