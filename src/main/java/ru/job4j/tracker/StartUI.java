c821445bc2663deb0bf2d5cd292b4380a752fe63
Kiril-k23 <kirillkleyn@gmail.com>
2023-05-15 23:02:10 +0300

    3.2. Реализация класса StartUI. Вывод всех заявок [#500745]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,6 +1,5 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class StartUI {
@@ -19,6 +18,16 @@ public class StartUI {
                 System.out.println("Добавленная заявка: " + item);
             } else if (select == 6) {
                 run = false;
+            } else if (select == 1) {
+                System.out.println("===Show all items===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище ещё не содержит заявок");
+                }
             }
         }
     }
