8409840596f1b27b542e14adb83c6d057e2e6f87
Kiril-k23 <kirillkleyn@gmail.com>
2023-06-01 00:07:53 +0300

    4. Телефонный справочник на базе ArrayList [#41598  [#41598]]

---
+++ src/main/java/ru/job4j/collection/Person.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String phone;
+    private String address;
+
+    public Person(String name, String surname, String phone, String address) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
