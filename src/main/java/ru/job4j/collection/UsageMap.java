a482be6d356b14a8c83172e47d1c3393e5c2226c
Kiril-k23 <kirillkleyn@gmail.com>
2023-06-29 20:31:25 +0300

    0. Map, HashMap [#212652  [#212652 #426299]]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> mail = new HashMap<>();
         mail.put("kirillkleyn@gmaim.com", "Kleyn Kirill");
+        mail.put("kirillkleyn@gmaim.com", "Kleyn Kirill");
+        mail.put("inanovstr@gmaim.com", "Ivan Ivanov");
         for (String key : mail.keySet()) {
             String value = mail.get(key);
             System.out.println(key + " : " + value);
